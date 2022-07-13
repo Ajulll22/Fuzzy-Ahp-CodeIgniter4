2f4f596975316bd5e3a55189aa7914b33ad98a06
Ajulll22 <ajulrizki@gmail.com>
2022-07-13 17:17:46 +0700

    auth filter

---
+++ app/Filters/AuthFilter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class AuthFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if(! session()->get('login')) {
+            echo "invalid";
+            return redirect()->to(base_url('/auth'))->with('error', "Invalid Credential");
+        }
+        // Do something here
+    }
+
+    //--------------------------------------------------------------------
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do something here
+    }
+}
