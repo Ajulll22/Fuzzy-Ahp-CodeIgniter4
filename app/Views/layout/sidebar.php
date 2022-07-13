e21d93f51d6cc84f209282b126684cdf9c179bfe
Ajulll22 <ajulrizki@gmail.com>
2022-07-13 17:18:36 +0700

    sidebar update

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <center>
-            <span class="brand-text font-weight-light"><b>Sistem Pendukung Keputusan</b></span><br>
+            <span class="brand-text font-weight-light"><b>SPK</b></span><br>
             <span class="brand-text font-weight-light"><b>Penilaian Kualitas Produk</b></span>
         </center>
     </a>
