7208d9f98dad6000a59e1668e13f6dfb876edf66
Ajulll22 <ajulrizki@gmail.com>
2022-07-13 17:17:34 +0700

    dashboard update

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,14 +1,31 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\SubkriteriaModel;
+use App\Models\KriteriaModel;
+use App\Models\AlternatifModel;
 
 class Dashboard extends BaseController
 {
+    public function __construct() 
+    {
+        $this->kriteria = new KriteriaModel();
+        $this->subKriteria = new SubkriteriaModel();
+        $this->alternatif = new AlternatifModel(); 
+    }
+
     public function index()
     {
+        $kriteria = $this->kriteria->get()->getResultArray();
+        $subKriteria = $this->subKriteria->get()->getResultArray();
+        $alternatif = $this->alternatif->get()->getResultArray();
+
         $data = [
             'title' => "FAHP - Dashboard",
-            'head' => "Dashboard",        
+            'head' => "Dashboard",
+            'count_krit' => count($kriteria),
+            'count_sub' => count($subKriteria),
+            'count_alternatif' => count($alternatif),        
         ];
 
         return view('dashboard', $data);
