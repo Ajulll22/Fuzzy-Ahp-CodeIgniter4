c3708d3088878766954c6a111d82ffc07f4ffec9
Ajulll22 <ajulrizki@gmail.com>
2022-07-13 17:17:19 +0700

    routes update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/login', 'Auth::login');
+
+$routes->get('/Dashboard', 'Dashboard::index', ['filter' => 'authfilter']);
 /*
  * --------------------------------------------------------------------
  * Additional Routing
